f97b3ec44b723b49f2269974bc3eb1a30012e085
Joe Spall <jspall0327@gmail.com>
2020-11-19 20:05:26 -0500

    Updates cal_collection for final use

--- src/cal_collection.cpp
+++ src/cal_collection.cpp
@@ -1,28 +1,12 @@
 // Copyright (C) 2019 Eugene a.k.a. Realizator, stereopi.com, virt2real team
-// Ported from Python to C++ by Konstantin Ozernov on 10/10/2019.
-//
-// This file is part of StereoPi С++ tutorial scripts, and has been
-// ported from Pyton version (https://github.com/realizator/stereopi-fisheye-robot)
-//
-// StereoPi tutorial is free software: you can redistribute it 
-// and/or modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation, either version 3 of the 
-// License, or (at your option) any later version.
-//
-// StereoPi tutorial is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with StereoPi tutorial.  
-// If not, see <http://www.gnu.org/licenses/>.
+// Ported from Python to C++ by Konstantin Ozernov on 10/10/2019. 
+// 
+// Modifications for distributed-vision project by Joseph Spall IV
 //
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-//#include <stdio.h>
 #include <unistd.h>
 
 
@@ -52,23 +36,20 @@ int main()
     int half_height = 480;
     
     //Final image capture settings
-    float scale_ratio = 1;//
+    float scale_ratio = 1;
 
-        // Camera resolution height must be dividable by 16, and width by 32
+    // Camera resolution height must be dividable by 16, and width by 32
         
     cam_width = (int)(((cam_width+31)/32)*32);
     cam_height = (int)(((cam_height+15)/16)*16);
     fprintf(stderr, "Camera resolution: %d x %d\n", cam_width, cam_height);
 
-        // Buffer for captured image settings
+    // Buffer for captured image settings
     int img_width = (int)((cam_width * scale_ratio));
     int img_height = (int)((cam_height * scale_ratio));
-    //    capture = np.zeros((img_height, img_width, 4), dtype=np.uint8)
     fprintf(stderr, "Scaled image resolution: %d x %d\n", img_width, img_height);
 
-        // Initialize the camera
-    //cv::VideoCapture cap(0);//"/dev/stdin");
-
+    // Initialize the camera
     FILE *fp;
     if ((fp = fopen("/dev/stdin", "rb")) == NULL)
     {
@@ -80,24 +61,21 @@ int main()
     char *buf = (char *)malloc(bufLen);
     int count = 0;
 
-    
-        // Lets start taking photos!
+    // Lets start taking photos!
     int counter = 0;
     long long t2 = getTimestamp();
     fprintf(stderr, "Starting photo sequence\n");
     cv::Mat frame;
-    //    while ((count = fread(buf, sizeof(*buf), bufLen, fp)) != 0)
     while (true)
     {
         fseek(fp, -bufLen, SEEK_END);
         count = fread(buf, sizeof(*buf), bufLen, fp);
-	if (count == 0)
-	  break;
+	    if (count == 0)
+	        break;
         cv::Mat frame(cam_height, cam_width, CV_8UC1, buf);
         long long t1 = getTimestamp();
         int cntdwn_timer = countdown - (int)((t1-t2) / 1000000);
 	
-            
         // If cowntdown is zero - let's record next image
         if (cntdwn_timer == -1)
         {
@@ -106,12 +84,12 @@ int main()
             cv::imwrite(filename, frame);
             fprintf(stderr, "[%d of %d] %s\n", counter, total_photos, filename.c_str());
 
-	    cv::Mat imgLeft = cv::Mat(frame, cv::Rect(0, 0 , half_width, half_height));
-	    cv::Mat imgRight = cv::Mat(frame, cv::Rect(half_width, 0, half_width, half_height));
-	    std::string leftName = folder_name + "pairs/left_" + std::to_string(counter) + ".png";
-	    std::string rightName = folder_name + "pairs/right_" + std::to_string(counter) + ".png";
-	    cv::imwrite(leftName, imgLeft);
-	    cv::imwrite(rightName, imgRight);
+	        cv::Mat img_front = cv::Mat(frame, cv::Rect(0, 0 , half_width, half_height));
+	        cv::Mat img_back = cv::Mat(frame, cv::Rect(half_width, 0, half_width, half_height));
+	        std::string front_name = folder_name + "pairs/front_" + std::to_string(counter) + ".png";
+	        std::string back_name = folder_name + "pairs/back_" + std::to_string(counter) + ".png";
+	        cv::imwrite(front_name, img_front);
+	        cv::imwrite(back_name, img_back);
 	    
             t2 = getTimestamp();
             sleep(1);
